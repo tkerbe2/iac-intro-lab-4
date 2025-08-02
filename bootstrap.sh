08f1bb36c18991f6096cb3262f8f258685693ccf
Taylor <69871115+tkerbe2@users.noreply.github.com>
2025-08-02 16:34:04 -0500

    name change for repo in shell script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,8 @@ yum install python3 -y
 yum install python3-pip -y
 python3 -m pip install simple_http_server -y
 yum install git -y
-git clone https://github.com/tkerbe2/tkdev-terraform-web-app
-cd tkdev-terraform-web-app
+git clone https://github.com/tkerbe2/iac-intro-lab-4
+cd iac-intro-lab-4
 chmod 700 bash-html-create.sh
 ./bash-html-create.sh
 python3 -m http.server 80 >> output.html
